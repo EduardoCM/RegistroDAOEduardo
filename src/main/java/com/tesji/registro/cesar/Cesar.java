4726ab38e356b47bac76e5fff6a151571f55ff5a
Cesar Giovani Cruz Piña <undertaker_400@hotmail.com>
2018-02-03 14:13:06 -0600

    mi primer commit

---
+++ src/main/java/com/tesji/registro/cesar/Cesar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tesji.registro.cesar;
+
+/**
+ *
+ * @author UNDER
+ */
+public class Cesar {
+    private String nombre;
+    private String direccion;
+    private int telefono;
+    private int edad;
+}
