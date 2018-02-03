f91d0251418a1d5c940a48e08c56064eee4736d4
JorgeBrito <bmjordy@yahoo.com.mx>
2018-02-03 14:03:02 -0600

    Brito Class

---
+++ src/main/java/com/tesji/registro/brito/Brito.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tesji.registro.brito;
+
+/**
+ *
+ * @author Amigos
+ */
+public class Brito {
+
+private String carrera;
+private String escuela;
+}
